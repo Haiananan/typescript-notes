9f0c9f8ba40b4afbe1dec05e63dcb6d3442e2ee8
海 <1628089948@qq.com>
2023-01-03 23:29:19 +0800

    v2

--- demo.ts
+++ demo.ts
@@ -1,8 +1,15 @@
-type StrLen<
-  Str extends string,
-  Count extends any[] = []
-> = Str extends `${infer First}${infer Rest}`
-  ? StrLen<Rest, [...Count, any]>
-  : Count["length"];
+type GreaterThan<
+  A extends number,
+  B extends number,
+  Counter extends any[] = []
+> = A extends B
+  ? false
+  : A extends Counter["length"]
+  ? false
+  : B extends Counter["length"]
+  ? true
+  : GreaterThan<A, B, [...Counter, any]>;
 
-type res = StrLen<"beyond">; // type res = 6
+type res = GreaterThan<3, 2>; // true
+type res2 = GreaterThan<3, 3>; // false
+type res3 = GreaterThan<3, 4>; // false
--- note.md
+++ note.md
@@ -756,3 +756,25 @@ type StrLen<
 
 type res = StrLen<"beyond">; // type res = 6
 ```
+
+### 比较大小
+
+核心思路：元组计数，每次加一后跟AB对比，先等于哪个，说明哪个更小。
+
+```ts
+type GreaterThan<
+  A extends number,
+  B extends number,
+  Counter extends any[] = []
+> = A extends B
+  ? false
+  : A extends Counter["length"]
+  ? false
+  : B extends Counter["length"]
+  ? true
+  : GreaterThan<A, B, [...Counter, any]>;
+
+type res = GreaterThan<3, 2>; // true
+type res2 = GreaterThan<3, 3>; // false
+type res3 = GreaterThan<3, 4>; // false
+```
